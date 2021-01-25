0d17ef6d32d3e6daeb75bbaacbc441ff5d5c01a8
Josh Wulf <josh@magikcraft.io>
2021-01-25 19:45:53 +1300

    Update packages
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/lcd.ts
+++ src/lcd.ts
@@ -3,7 +3,7 @@ export class LCD {
   lcd: any;
   constructor() {
     const five = require("johnny-five");
-    const raspi = require("raspi-io");
+    const raspi = require("raspi-io").RaspIO;
 
     console.log("Starting j5...");
     this.board = new five.Board({ repl: false, io: new raspi() });
