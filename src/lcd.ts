a6af8cff99a6c9cf4d990cd214d2903e28a79f08
Josh Wulf <josh@magikcraft.io>
2021-01-26 18:53:20 +1300

    Update GPIO pins
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/lcd.ts
+++ src/lcd.ts
@@ -11,12 +11,13 @@ export class LCD {
     this.board.on("ready", () => {
       this.log("Board ready...");
       this.lcd = new five.LCD({
+        // pin layout for Zero W: https://pi4j.com/1.2/pins/model-zerow-rev1.html
         // lcd pins: ["GND", "VDD", "VO",  "RS", "RW", "EN", "DB0", "DB1", "DB2", "DB3", "DB4", "DB5", "DB6", "DB7", "LED+", "LED-"]
-        // lcd pin - func -  rpi pin - func
+        // lcd pin - func -  rpi pin - func         -    note
         //    1      GND       6       GND
         //    2      VDD       1       +5V
         //    3      VO       pot      contrast
-        //    4      RS        12      GPIO18
+        //    4      RS        12      GPIO18          Register Select
         //    5      RW        14      GND
         //    6      EN        16      GPIO23
         //    7      DB0       -       -
@@ -35,7 +36,8 @@ export class LCD {
         // lcd pins:   [ 4,          6,        11,      12,       13,       14  ]
         // raspi pins: ["GPIO18", "GPIO23", "GPIO24", "GPIO27", "GPIO25", "GPIO22"]
         // raspi pins: ["12",       "16",     "18",   "13",     "22",      "15"]
-        pins: ["GPIO18", "GPIO23", "GPIO24", "GPIO27", "GPIO25", "GPIO22"],
+        // pins: ["GPIO18", "GPIO23", "GPIO24", "GPIO27", "GPIO25", "GPIO22"],
+        pins: ["GPIO1", "GPIO4", "GPIO5", "GPIO2", "GPIO6", "GPIO3"],
         backlight: 6,
         rows: 2,
         cols: 20,
