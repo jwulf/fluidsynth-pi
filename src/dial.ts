3d34dc91da75c83409a372166bb48376c022e3b9
Josh Wulf <josh@magikcraft.io>
2021-01-27 22:48:50 +1300

    move pin
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/dial.ts
+++ src/dial.ts
@@ -14,7 +14,7 @@ export class Dial {
   constructor() {
     this.log = Log(chalk.greenBright);
     board().ready.then((board) => {
-      this.upButton = new five.Button("GPIO2");
+      this.upButton = new five.Button("GPIO4");
       this.downButton = new five.Button("GPIO3");
       this.pressButton = new five.Button({ pin: "GPIO6" });
       rotaryEncoder({
