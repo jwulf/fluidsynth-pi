720590e66bd3d3559bb41521f72bc97ce30d09ab
Josh Wulf <josh@magikcraft.io>
2021-01-29 14:12:20 +1300

    use pin instead of button
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- dist/dial.js
+++ dist/dial.js
@@ -8,22 +8,29 @@ const chalk_1 = __importDefault(require("chalk"));
 const johnny_five_1 = __importDefault(require("johnny-five"));
 const board_1 = require("./board");
 const ringlog_1 = require("./ringlog");
+function rotaryEncoder({ aPin, bPin, pressButton, onUp, onDown, onPress, }) {
+    aPin.on("low", () => {
+        const bPinState = bPin.value;
+        console.log(`aPin: low, bPin: ${bPinState}`);
+    });
+    bPin.on("low", () => {
+        const aPinState = aPin.value;
+        console.log(`bPin: low, aPin: ${aPinState}`);
+    });
+    pressButton.on("up", () => {
+        onPress();
+    });
+}
 class Dial {
     constructor() {
         this.log = ringlog_1.Log(chalk_1.default.greenBright);
-        board_1.board().ready.then((board) => {
-            this.upButton = new johnny_five_1.default.Button({
-                pin: "GPIO4",
-                isPullup: true,
-            });
-            this.downButton = new johnny_five_1.default.Button({
-                pin: "GPIO5",
-                isPullup: true,
-            });
+        board_1.board().ready.then(() => {
+            const aPin = new johnny_five_1.default.Pin("GPIO4");
+            const bPin = new johnny_five_1.default.Pin("GPIO5");
             this.pressButton = new johnny_five_1.default.Button({ pin: "GPIO6", isPullup: true });
             rotaryEncoder({
-                upButton: this.upButton,
-                downButton: this.downButton,
+                aPin,
+                bPin,
                 pressButton: this.pressButton,
                 onUp: () => {
                     console.log("up");
@@ -39,36 +46,85 @@ class Dial {
     }
 }
 exports.Dial = Dial;
-function rotaryEncoder({ upButton, downButton, pressButton, onUp, onDown, onPress, }) {
-    let waveform = "";
-    let waveformTimeout;
-    upButton.on("up", () => {
-        waveform += "1";
-        handleWaveform();
-    });
-    downButton.on("up", () => {
-        waveform += "0";
-        handleWaveform();
-    });
-    pressButton.on("up", () => {
-        onPress();
-    });
-    function handleWaveform() {
-        if (waveform.length < 2) {
-            waveformTimeout = setTimeout(() => {
-                waveform = "";
-            }, 8);
-            return;
-        }
-        if (waveformTimeout) {
-            clearTimeout(waveformTimeout);
-        }
-        if (waveform === "01") {
-            onUp();
-        }
-        else if (waveform === "10") {
-            onDown();
-        }
-        waveform = "";
-    }
-}
+// export class Dial {
+//   dial!: five.LCD;
+//   log: (msg: string) => void;
+//   board: any;
+//   upButton!: five.Button;
+//   downButton!: five.Button;
+//   pressButton!: five.Button;
+//   constructor() {
+//     this.log = Log(chalk.greenBright);
+//     board().ready.then((board) => {
+//       this.upButton = new five.Pin({
+//         pin: "GPIO4",
+//         isPullup: true,
+//       });
+//       this.downButton = new five.Button({
+//         pin: "GPIO5",
+//         isPullup: true,
+//       });
+//       this.pressButton = new five.Button({ pin: "GPIO6", isPullup: true });
+//       rotaryEncoder({
+//         upButton: this.upButton,
+//         downButton: this.downButton,
+//         pressButton: this.pressButton,
+//         onUp: () => {
+//           console.log("up");
+//         },
+//         onDown: () => {
+//           console.log("down");
+//         },
+//         onPress: () => {
+//           console.log("press");
+//         },
+//       });
+//     });
+//   }
+// }
+// function rotaryEncoder({
+//   upButton,
+//   downButton,
+//   pressButton,
+//   onUp,
+//   onDown,
+//   onPress,
+// }: {
+//   upButton: five.Button;
+//   downButton: five.Button;
+//   pressButton: five.Button;
+//   onUp: () => void;
+//   onDown: () => void;
+//   onPress: () => void;
+// }) {
+//   let waveform = "";
+//   let waveformTimeout: NodeJS.Timeout;
+//   upButton.on("up", () => {
+//     waveform += "1";
+//     handleWaveform();
+//   });
+//   downButton.on("up", () => {
+//     waveform += "0";
+//     handleWaveform();
+//   });
+//   pressButton.on("up", () => {
+//     onPress();
+//   });
+//   function handleWaveform() {
+//     if (waveform.length < 2) {
+//       waveformTimeout = setTimeout(() => {
+//         waveform = "";
+//       }, 4);
+//       return;
+//     }
+//     if (waveformTimeout) {
+//       clearTimeout(waveformTimeout);
+//     }
+//     if (waveform === "01") {
+//       onUp();
+//     } else if (waveform === "10") {
+//       onDown();
+//     }
+//     waveform = "";
+//   }
+// }
--- src/dial.ts
+++ src/dial.ts
@@ -3,28 +3,52 @@ import five from "johnny-five";
 import { board } from "./board";
 import { Log } from "./ringlog";
 
+function rotaryEncoder({
+  aPin,
+  bPin,
+  pressButton,
+  onUp,
+  onDown,
+  onPress,
+}: {
+  aPin: five.Pin;
+  bPin: five.Pin;
+  pressButton: five.Button;
+  onUp: () => void;
+  onDown: () => void;
+  onPress: () => void;
+}) {
+  aPin.on("low", () => {
+    const bPinState = bPin.value;
+    console.log(`aPin: low, bPin: ${bPinState}`);
+  });
+
+  bPin.on("low", () => {
+    const aPinState = aPin.value;
+    console.log(`bPin: low, aPin: ${aPinState}`);
+  });
+
+  pressButton.on("up", () => {
+    onPress();
+  });
+}
+
 export class Dial {
   dial!: five.LCD;
   log: (msg: string) => void;
   board: any;
-  upButton!: five.Button;
-  downButton!: five.Button;
+  upButton!: five.Pin;
+  downButton!: five.Pin;
   pressButton!: five.Button;
   constructor() {
     this.log = Log(chalk.greenBright);
-    board().ready.then((board) => {
-      this.upButton = new five.Button({
-        pin: "GPIO4",
-        isPullup: true,
-      });
-      this.downButton = new five.Button({
-        pin: "GPIO5",
-        isPullup: true,
-      });
+    board().ready.then(() => {
+      const aPin = new five.Pin("GPIO4");
+      const bPin = new five.Pin("GPIO5");
       this.pressButton = new five.Button({ pin: "GPIO6", isPullup: true });
       rotaryEncoder({
-        upButton: this.upButton,
-        downButton: this.downButton,
+        aPin,
+        bPin,
         pressButton: this.pressButton,
         onUp: () => {
           console.log("up");
@@ -40,56 +64,93 @@ export class Dial {
   }
 }
 
-function rotaryEncoder({
-  upButton,
-  downButton,
-  pressButton,
-  onUp,
-  onDown,
-  onPress,
-}: {
-  upButton: five.Button;
-  downButton: five.Button;
-  pressButton: five.Button;
-  onUp: () => void;
-  onDown: () => void;
-  onPress: () => void;
-}) {
-  let waveform = "";
-  let waveformTimeout: NodeJS.Timeout;
+// export class Dial {
+//   dial!: five.LCD;
+//   log: (msg: string) => void;
+//   board: any;
+//   upButton!: five.Button;
+//   downButton!: five.Button;
+//   pressButton!: five.Button;
+//   constructor() {
+//     this.log = Log(chalk.greenBright);
+//     board().ready.then((board) => {
+//       this.upButton = new five.Pin({
+//         pin: "GPIO4",
+//         isPullup: true,
+//       });
+//       this.downButton = new five.Button({
+//         pin: "GPIO5",
+//         isPullup: true,
+//       });
+//       this.pressButton = new five.Button({ pin: "GPIO6", isPullup: true });
+//       rotaryEncoder({
+//         upButton: this.upButton,
+//         downButton: this.downButton,
+//         pressButton: this.pressButton,
+//         onUp: () => {
+//           console.log("up");
+//         },
+//         onDown: () => {
+//           console.log("down");
+//         },
+//         onPress: () => {
+//           console.log("press");
+//         },
+//       });
+//     });
+//   }
+// }
 
-  upButton.on("up", () => {
-    waveform += "1";
-    handleWaveform();
-  });
+// function rotaryEncoder({
+//   upButton,
+//   downButton,
+//   pressButton,
+//   onUp,
+//   onDown,
+//   onPress,
+// }: {
+//   upButton: five.Button;
+//   downButton: five.Button;
+//   pressButton: five.Button;
+//   onUp: () => void;
+//   onDown: () => void;
+//   onPress: () => void;
+// }) {
+//   let waveform = "";
+//   let waveformTimeout: NodeJS.Timeout;
 
-  downButton.on("up", () => {
-    waveform += "0";
-    handleWaveform();
-  });
+//   upButton.on("up", () => {
+//     waveform += "1";
+//     handleWaveform();
+//   });
 
-  pressButton.on("up", () => {
-    onPress();
-  });
+//   downButton.on("up", () => {
+//     waveform += "0";
+//     handleWaveform();
+//   });
 
-  function handleWaveform() {
-    if (waveform.length < 2) {
-      waveformTimeout = setTimeout(() => {
-        waveform = "";
-      }, 8);
-      return;
-    }
+//   pressButton.on("up", () => {
+//     onPress();
+//   });
 
-    if (waveformTimeout) {
-      clearTimeout(waveformTimeout);
-    }
+//   function handleWaveform() {
+//     if (waveform.length < 2) {
+//       waveformTimeout = setTimeout(() => {
+//         waveform = "";
+//       }, 4);
+//       return;
+//     }
 
-    if (waveform === "01") {
-      onUp();
-    } else if (waveform === "10") {
-      onDown();
-    }
+//     if (waveformTimeout) {
+//       clearTimeout(waveformTimeout);
+//     }
 
-    waveform = "";
-  }
-}
+//     if (waveform === "01") {
+//       onUp();
+//     } else if (waveform === "10") {
+//       onDown();
+//     }
+
+//     waveform = "";
+//   }
+// }
