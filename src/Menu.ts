21efffb0bafcd2cd609c86ee30282b65ed939f18
Josh Wulf <josh@magikcraft.io>
2021-02-03 11:44:09 +1300

    Remove .sf2 in UI
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/Menu.ts
+++ src/Menu.ts
@@ -72,7 +72,12 @@ export class Menu {
         this.showLoadingMessage();
         this.fluidsynth
           .loadPreviousFont()
-          .then(() => this.lcdPrint(this.fluidsynth.currentSoundFont, 0));
+          .then(() =>
+            this.lcdPrint(
+              this.fluidsynth.currentSoundFont.replace(".sf2", ""),
+              0
+            )
+          );
         break;
       }
       case "SYSTEM": {
@@ -91,7 +96,12 @@ export class Menu {
         this.showLoadingMessage();
         this.fluidsynth
           .loadNextFont()
-          .then(() => this.lcdPrint(this.fluidsynth.currentSoundFont, 0));
+          .then(() =>
+            this.lcdPrint(
+              this.fluidsynth.currentSoundFont.replace(".sf2", ""),
+              0
+            )
+          );
         break;
       }
       case "SYSTEM": {
@@ -294,7 +304,7 @@ class FontScroller {
   }
 
   private printFont() {
-    const msg = this.fonts[this.index].padEnd(14, " ");
+    const msg = this.fonts[this.index].replace(".sf2", "").padEnd(14, " ");
     this.lcdPrint(`:arrowright: ${msg}`, 0);
   }
 }
